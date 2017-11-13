831a4818ce874d0a9fb8adc553b8faeb95afc615
bruno08 <bruno.quinquis@ville-noumea.nc>
2017-11-13 14:25:57 +1100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 #
 # This class installs and configures a Symantec BackupExec agent on Linux
 #
-#
+
 class backupexec (
     $pkgname = $backupexec::params::pkgname,
 ) inherits backupexec::params {
