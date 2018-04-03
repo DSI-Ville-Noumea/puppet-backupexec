432523fa56ab1c05a017d6ced1418ae5a27d759f
Bruno <bruno.quinquis@ville-noumea.nc>
2018-04-04 10:55:49 +1100

    #28430 passwd beuser 2

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,6 +6,7 @@ class backupexec::params {
   case $::osfamily {
     'RedHat': {
       $pkgname = 'VRTSralus'
+      $password = indef
     }
     default: {
       fail ("Your OS : ${::operatingsystem} is not supported.")
