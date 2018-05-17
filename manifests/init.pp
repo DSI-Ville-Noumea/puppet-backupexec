4128b65dd1dd9252f17ad8549bf39fa5a1f7828b
Mélanie <MelanieGault@users.noreply.github.com>
2018-05-17 14:32:42 +1100

    debut du changement sur le ralus.cfg

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,7 +23,32 @@ class backupexec (
     ensure  => latest,
     require => User['beuser'],
   }
-
+  
+  file { '/etc/VRTSralus/ralus.cfg':
+    ensure  => file,
+    owner   => 'beuser',
+    group   => 'beoper',
+    mode    => '0644',
+    require => Package[$backupexec::params::pkgname],
+    notify  => Service['VRTSralus.init'],
+  }
+  file_line { 'Software\\Symantec\\Backup Exec For Windows\\Agent Browser\\TcpIp\\AdvertisementPort' :
+    ensure => present,
+    path   => '/etc/VRTSralus/ralus.cfg',
+    line   => 'Software\\Symantec\\Backup Exec For Windows\\Agent Browser\\TcpIp\\AdvertisementPort=6101',
+    match  => '^Software\\Symantec\\Backup\ Exec\ For\ Windows\\Agent\ Browser\\TcpIp\\AdvertisementPort\=',
+    require => Package[$backupexec::params::pkgname],
+    notify  => Service['VRTSralus.init'],
+  }
+  file_line { 'Software\\Symantec\\Backup Exec For Windows\\Backup Exec\\Debug\\AgentConfig' :
+    ensure => present,
+    path   => '/etc/VRTSralus/ralus.cfg',
+    line   => 'Software\\Symantec\\Backup Exec For Windows\\Backup Exec\\Debug\\AgentConfig=0',
+    match  => '^Software\\Symantec\\Backup\ Exec\ For\ Windows\\Backup\ Exec\\Debug\\AgentConfig\=',
+    require => Package[$backupexec::params::pkgname],
+    notify  => Service['VRTSralus.init'],
+  }
+  
   #file { '/etc/VRTSralus/ralus.cfg':
   #  ensure  => file,
   #  owner   => 'beuser',
