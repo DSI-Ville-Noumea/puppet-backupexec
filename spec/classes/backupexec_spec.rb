d557dcb083200f22db4ed73e6178e729b58c2198
Bruno <bruno.quinquis@ville-noumea.nc>
2018-05-23 10:36:00 +1100

    #46045

--- spec/classes/backupexec_spec.rb
+++ spec/classes/backupexec_spec.rb
@@ -9,11 +9,11 @@ describe 'backupexec' do
   context 'with hiera config on RedHat' do
     let(:hiera_config) { hiera_config }
     let :facts do
-
       {
         osfamily: 'RedHat'
       }
     end
+
     it { is_expected.to contain_group('beoper') }
     it { is_expected.to contain_user('beuser').with(groups: 'beoper') }
     it { is_expected.to contain_file('/etc/VRTSralus/ralus.cfg') }
